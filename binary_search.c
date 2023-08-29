2f5a58ba3d0f56dec33827ef8ca65747b85cb1de
Hashini2004 <113332204+Hashini2004@users.noreply.github.com>
2023-08-29 09:22:59 +0530

    Add files via upload

--- binary_search.c
+++ binary_search.c
@@ -1,28 +1,43 @@
 #include<stdio.h>
+int binarysearch(int a[],int b,int e,int v)
+{
+	int mid;
+	if(e>=b)
+	{
+		mid=(b+e)/2;
+		if(a[mid]==v)
+		{
+			return mid+1;
+		}
+		else if(a[mid]<v)
+		{
+			return binarysearch(a,mid+1,e,v);
+		}
+		else
+		{
+			return binarysearch(a,e,mid-1,v);
+		}
+	}
+	return 1;
+}
 int main()
 {
-	int n,i,ele,a[100],c=0;
-	printf("Enter the number of elements: ");
+	int n,a[100],mid,i,ele;
+	printf("Enter the array size: ");
 	scanf("%d",&n);
-	for (i=0;i<n;i++)
+	for(i=0;i<n;i++)
 	{
 		scanf("%d",&a[i]);
 	}
-	printf("Enter the element to be found: ");
+	printf("Enter the element to be searched: ");
 	scanf("%d",&ele);
-	for(i=0;i<n;i++)
+	mid=binarysearch(a,0,n-1,ele);
+	if(mid==1)
 	{
-		if(ele==a[i])
-		{
-			c=1;
-		}
-	}
-	if(c==1)
-	{
-		printf("Element is found: %d",ele);
+		printf("Element is not found");
 	}
 	else
 	{
-		printf("Element is not found");
+		printf("Elemetnt is found at the %d index",mid);
 	}
 }
